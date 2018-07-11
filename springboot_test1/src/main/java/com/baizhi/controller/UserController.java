d5798438336be07f59790f9358e5190399cc5c64
liy <378606053@qq.com>
2018-07-11 16:17:30 +0800

    写了个测试的user控制器

---
+++ springboot_test1/src/main/java/com/baizhi/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.baizhi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    public void login(){
+
+    }
+    public void regist(){
+
+    }
+}
